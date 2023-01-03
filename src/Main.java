e8ef36b71cd8ed500d27b74cdc34423807e658d2
Koloshko Evgeni <kolohkoe@gmail.com>
2023-01-03 12:03:41 +0700

    Homework is ready

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        
+        int bonus_rub = 100;
+        int client_account = 120;
+        int refill = 1500;
 
+        if (refill >= 1100) {
+            client_account = client_account + refill + (refill / bonus_rub);
+        } else {
+            client_account = client_account + refill;
 
-
+        }
+        System.out.println("На счету " + client_account);
 
 
     }
