7932313c512602f56ba5a5f7417a82e24a466fd3
Koloshko Evgeni <kolohkoe@gmail.com>
2023-01-04 13:19:09 +0700

    The function is ready Bonusmilesservice

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,3 +1,10 @@
 public class BonusMilesService {
-    
+    public int calculate(int cost) {
+        int rub_for_a_miles = 20;
+        int bonus_miles = (cost / rub_for_a_miles);
+        return bonus_miles;
+
+    }
+
+
 }
